ae7ec5b63f107912afb656a55a0b0186a01d2fb7
Alberto Planas <aplanas@suse.com>
2020-12-14 14:39:10 +0100

    Drop Python 2.7 from the venv

--- venvjail.py
+++ venvjail.py
@@ -357,8 +357,6 @@ def create(args):
     options = []
     if args.system_site_packages:
         options.append("--system-site-packages")
-    # Make sure that we generate a Python 2.7 environment
-    options.append("--python=python2.7")
     options = " ".join(options)
     subprocess.call(f"virtualenv {options} {args.dest_dir}", shell=True)
 
